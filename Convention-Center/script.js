8058d54a18ce4bb84473aa52f1e05802cd88f3d2
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-10-24 23:59:00 +0600

    margin, padding fixes and floating item fix

---
+++ Convention-Center/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const toast = document.getElementById("toast")
+    // toast.addEventListener("click", toast.style.display = "none")
+});
+
+
